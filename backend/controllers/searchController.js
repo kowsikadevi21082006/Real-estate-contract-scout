f0f4d2e205a4205c1406e97f6cec8901e7f32c1a
kowsikadevi21082006 <deva36rp@gmail.com>
2025-12-31 12:36:10 +0530

    Switch to Cerebras embeddings

--- backend/controllers/searchController.js
+++ backend/controllers/searchController.js
@@ -1,4 +1,4 @@
-const { OpenAIEmbeddings } = require("@langchain/openai");
+const { CerebrasEmbeddings } = require("@langchain/cerebras");
 const { ChatCerebras } = require("@langchain/cerebras");
 const { MongoDBAtlasVectorSearch } = require("@langchain/mongodb");
 const mongoose = require("mongoose");
@@ -14,9 +14,8 @@ exports.compareContracts = async (req, res) => {
 
         // 1. Setup Vector Store
         const collection = mongoose.connection.db.collection("contracts");
-        const vectorStore = new MongoDBAtlasVectorSearch(new OpenAIEmbeddings({
-            modelName: "text-embedding-3-small",
-            apiKey: process.env.OPENAI_API_KEY
+        const vectorStore = new MongoDBAtlasVectorSearch(new CerebrasEmbeddings({
+            apiKey: process.env.CEREBRAS_API_KEY
         }), {
             collection,
             indexName: "default",
